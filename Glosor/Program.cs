e48d3dd0e37195b93b9718aefe07efa5c7839431
jens l <jelu000@gmail.com>
2019-05-19 11:10:50 +0200

    Klar med själv glostetstet

--- Glosor/Program.cs
+++ Glosor/Program.cs
@@ -19,13 +19,10 @@ namespace Glosor
 		{
 			
 			
-			
-			
-			
 			ArrayList alla_glosor = new ArrayList();
 			string mata_in = "j";
 			int i = 0;
-			string temp_glosa;
+			
 			
 			
 			// TODO: Implement Functionality Here
@@ -42,16 +39,56 @@ namespace Glosor
 				
 				alla_glosor.Add(t_glosa);
 				
-				Console.WriteLine(ValueType"Vill du mata in en glosa till j/n? ");
+				Console.WriteLine("\nVill du mata in en glosa till j/n? ");
 				mata_in = Console.ReadLine();
 				
 				
 			}
 			
+			mata_in = "j";
+			
+			while (mata_in != "n"){
+				
+				Console.Clear();
+				Console.WriteLine("Nu startar glostestet!\n");
+				
+				int antal_ratt = 0;
+				
+				
+				for (i=0; i<alla_glosor.Count; i++){
+					
+					Glosa t_glosa = (Glosa)alla_glosor[i];
+					Console.WriteLine(t_glosa.SveGlosa);
+					
+					string t_svar = Console.ReadLine();
+					
+					if (t_glosa.EngGlosa == t_svar){
+						Console.WriteLine("Korrekt!\n");
+						antal_ratt++;
+					}
+					
+					else
+						Console.WriteLine("Fel! Rätt svar är: " + t_glosa.EngGlosa + "\n");
+										
+					
+				}
+				
+				if (alla_glosor.Count == antal_ratt)
+					Console.WriteLine("Grattis! Du hade alla rätt.\n");
+					
+				else
+					Console.WriteLine("Du hade " + antal_ratt + " av " + alla_glosor.Count + " möjliga rätt.\n");
+				
+				
+				Console.WriteLine("Vill du göra om glostetstet? j/n");
+				
+				
+				mata_in = Console.ReadLine();
+						
+			}
 			
 			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
+			//Console.ReadKey(true);
 		}
 	}
 }
