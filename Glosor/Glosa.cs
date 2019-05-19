e64636c804b5dfb6eb1b7c2aaee6569ae28bb3dd
jens l <jelu000@gmail.com>
2019-05-19 06:19:23 +0200

    first commit.

---
+++ Glosor/Glosa.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: jens
+ * Date: 2019-05-18
+ * Time: 15:12
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Glosor
+{
+	/// <summary>
+	/// Description of Glosa.
+	/// </summary>
+	public class Glosa
+	{
+		private string sve_glosa;
+		private string eng_glosa;
+		//private String sve_glosa, eng_glosa; //have to import System namespace
+		
+		
+		//Constructors
+		public Glosa()
+		{
+			sve_glosa = "";
+			eng_glosa = "";
+		}
+		
+		public Glosa(string tsve_glosa, string teng_glosa){
+			sve_glosa = tsve_glosa;
+			eng_glosa = teng_glosa;
+		}
+		
+		//setters
+		public void setGlosa(string tsve_glosa, string teng_glosa){
+			sve_glosa = tsve_glosa;
+			eng_glosa = teng_glosa;
+		}
+		
+		//getters
+		public string getSveGlosa(){
+			return sve_glosa;
+		}
+		
+		public string getEngGlosa(){
+			return eng_glosa;
+		}
+		
+	}
+}
---
+++ Glosor/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: jens
+ * Date: 2019-05-18
+ * Time: 15:07
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Glosor
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
